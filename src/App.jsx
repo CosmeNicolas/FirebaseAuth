8560cdbeace206d685dc2c29964d6d06bd1469fc
CosmeNicolas <nicomas7590@gmail.com>
2024-01-29 20:10:47 -0300

    agregue el componente route y el componente Login en mi carpeta components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap'
 import Footer from './components/Footer';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
     <Container className="main text-center">
      <h1>Auth with Firebase </h1>
     </Container>
     <Footer/>
+    </BrowserRouter>
     </>
   )
 }
