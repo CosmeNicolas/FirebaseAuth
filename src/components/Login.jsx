e65e6df731172657a4cfa498814c88314b63f2a1
CosmeNicolas <nicomas7590@gmail.com>
2024-01-29 20:48:37 -0300

    agregue un formcontrol para el nombre de usaurio

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,21 +5,27 @@ const Login = () => {
   return (
     <>
     <Container className='mt-5 container-login' >
-       <h1>Sing up</h1>
+       <h1 className='py-2 text-light'>Sing up</h1>
        <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+        {/* userName */}
+       <Form.Group className="mb-3 text-light" controlId="formBasicEmail">
+        <Form.Label>UserName</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" />
+      </Form.Group>
+
+        {/* email */}
+      <Form.Group className="mb-3 text-light" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+        {/* password */}
+      <Form.Group className="mb-3 text-light" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      
+      <Button className='my-2' variant="primary" type="submit">
         Submit
       </Button>
     </Form>
