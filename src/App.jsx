1eee47584cd5f2d94ea907b7689916bb3a2c3265
CosmeNicolas <nicomas7590@gmail.com>
2024-01-29 11:09:05 -0300

    inicialice el pryecto con firebase con auteunticacion

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap'
+import Footer from './components/Footer';
+
+function App() {
+
+
+  return (
+    <>
+    <Container className="main text-center">
+     <h1>Auth with Firebase </h1>
+    </Container>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="py-3 bg bg-dark text-light text-center">
+       &copy; Todos los derechos reservados
+    </footer>
+  )
+}
+
+export default Footer
